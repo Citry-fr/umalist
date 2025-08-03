bf73a2aa699277790c282694e2b7200b1d58d9fb
Tristan <citrynox9@gmail.com>
2025-08-03 13:27:57 +0200

    Add uma started

---
+++ renderer/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <v-tabs
+    v-model="tab"
+    align-tabs="center"
+    bg-color="grey-darken-3"
+    class="w-50 d-flex mx-auto rounded-b-lg"
+    color="deep-purple"
+    grow="true"
+  >
+    <v-tab value="one">
+      Add Uma
+    </v-tab>
+    <v-divider inset vertical />
+    <v-tab value="two">
+      Uma List
+    </v-tab>
+  </v-tabs>
+
+  <v-tabs-window v-model="tab" class=" w-50 mx-auto mt-4">
+    <v-tabs-window-item reverse-transition="tabs-window-item-reverse-transition" transition="tabs-window-item-transition" value="one">
+      <AddUmaComponent />
+    </v-tabs-window-item>
+    <v-tabs-window-item reverse-transition="tabs-window-item-reverse-transition" transition="tabs-window-item-transition" value="two">
+      <h1>Uma List</h1>
+    </v-tabs-window-item>
+  </v-tabs-window>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  import AddUmaComponent from '@/components/AddUmaComponent.vue'
+
+  const tab = ref(null)
+</script>
+
+<style scoped>
+
+</style>
---
+++ renderer/src/components/AddUmaComponent.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container">
+    <v-card class=" d-flex flex-column text-center ga-4">
+      <v-card-title class="text-h5">
+        Add Uma Musume
+      </v-card-title>
+      <v-card-subtitle class="text-h6">
+        Select Uma
+      </v-card-subtitle>
+      <div>uma dropdown</div>
+      <v-divider class="divider" opacity=".5" thickness="1" />
+      <v-card-subtitle class="text-h6">
+        Stats
+      </v-card-subtitle>
+      <div class="d-flex flex-row justify-center justify-space-evenly">
+        <div v-for="stat in stats" :key="stat.name">
+          <v-number-input
+            v-model="stat.value"
+            control-variant="stacked"
+            inset
+            :label="stat.name"
+            max="1200"
+            min="0"
+            min-width="130px"
+          />
+        </div>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const stats = ref([
+    {
+      name: 'Speed',
+      value: null,
+    },
+    {
+      name: 'Stamina',
+      value: null,
+    },
+    {
+      name: 'Power',
+      value: null,
+    },
+    {
+      name: 'Guts',
+      value: null,
+    },
+    {
+      name: 'Wisdom',
+      value: null,
+    },
+  ])
+
+</script>
+
+<style lang="scss" scoped>
+.container {
+ width: 100%;
+ height: 600px;
+}
+.divider {
+ margin: 0 15%;
+}
+</style>
